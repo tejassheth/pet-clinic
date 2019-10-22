73c2afd311904bf11ec63c31b6715a57638bf247
tejassheth <sheth.t.j.2012@gmail.com>
2019-10-22 01:54:02 -0400

    Updated Vet with JPA entity mappings. closes #39

--- pet-clinic-data/src/main/java/org/learn/petclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/org/learn/petclinic/model/Speciality.java
@@ -1,6 +1,13 @@
 package org.learn.petclinic.model;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialities")
 public class Speciality extends BaseEntity {
+    @JoinColumn(name = "description")
     private String description;
 
     public String getDescription() {
--- pet-clinic-data/src/main/java/org/learn/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/org/learn/petclinic/model/Vet.java
@@ -1,14 +1,21 @@
 package org.learn.petclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
-
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialities",joinColumns = @JoinColumn(name = "vet_id"),inverseJoinColumns = @JoinColumn(name = "specialitiy_id"))
     private Set<Speciality> specialities = new HashSet<>();
+
 
     public Set<Speciality> getSpecialities() {
         return specialities;
     }
+
 
     public void setSpecialities(Set<Speciality> specialities) {
         this.specialities = specialities;
