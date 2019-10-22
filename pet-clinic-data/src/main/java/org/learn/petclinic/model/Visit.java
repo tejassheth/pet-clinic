db99c1051c37fd0aa1e3b9b74c9b091288c95c08
tejassheth <sheth.t.j.2012@gmail.com>
2019-10-22 02:17:29 -0400

    added visit entity. closes #40

--- pet-clinic-data/src/main/java/org/learn/petclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/org/learn/petclinic/model/Visit.java
@@ -1,33 +1,25 @@
 package org.learn.petclinic.model;
 
-import java.time.LocalDate;
+import lombok.Getter;
+import lombok.Setter;
 
+import javax.persistence.*;
+import java.time.LocalDate;
+@Setter
+@Getter
+@Entity
+@Table(name = "visit")
 public class Visit extends BaseEntity {
+
+    @Column(name = "date")
     private LocalDate date;
+
+    @Column(name = "description")
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
-    public LocalDate getDate() {
-        return date;
-    }
 
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Pet getPet() {
-        return pet;
-    }
-
-    public void setPet(Pet pet) {
-        this.pet = pet;
-    }
 }
