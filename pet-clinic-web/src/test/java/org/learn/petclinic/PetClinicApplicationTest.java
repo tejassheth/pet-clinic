bca99c25f62afab506d6353bf656f0181eb213e6
tejassheth <sheth.t.j.2012@gmail.com>
2019-10-29 09:02:05 -0400

    Upgrade to JUnit 5 dependency . closes #61

---
+++ pet-clinic-web/src/test/java/org/learn/petclinic/PetClinicApplicationTest.java
@@ -0,0 +1,16 @@
+package org.learn.petclinic;
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+class PetClinicApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+
+}
